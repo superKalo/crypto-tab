3872d17188b15d30519ff14bee22079d784165e5
superKalo <superkalo@devlabs.bg>
2017-10-15 01:15:17 +0300

    Refactor: split the bitcoin related stuff in their own module

---
+++ js/bitcoin.js
@@ -0,0 +1,84 @@
+window.App = window.App || {};
+
+window.App.Bitcoin = {
+    PERIODS: {
+        ALL: 'ALL',
+        ONE_YEAR: 'ONE_YEAR',
+        ONE_MONTH: 'ONE_MONTH',
+        ONE_WEEK: 'ONE_WEEK',
+        ONE_DAY: 'ONE_DAY',
+        ONE_HOUR: 'ONE_HOUR'
+    },
+
+    $chart: document.getElementById('chart'),
+    chart: null,
+
+    $dataPeriods: document.querySelectorAll('.js-period'),
+    initEvents() {
+        const self = this;
+
+        [...self.$dataPeriods].forEach(el => {
+            el.addEventListener('click', function() {
+                self.$dataPeriods.forEach(p => p.classList.remove('active'));
+                this.classList.add('active');
+
+                const period = this.dataset.period;
+                self.repositories[period].getData()
+                    .then(_data => self.chart.init(_data));
+            });
+        })
+
+        self.$dataPeriods[0].click();
+    },
+
+    getBitcoinData(period) {
+        switch(period) {
+            case 'ALL':
+                return App.API.getBitcoinRatesForAll();
+            case 'ONE_YEAR':
+                return App.API.getBitcoinRatesForOneYear();
+            case 'ONE_MONTH':
+                return App.API.getBitcoinRatesForOneMonth();
+            case 'ONE_WEEK':
+                return App.API.getBitcoinRatesForOneWeek();
+            case 'ONE_DAY':
+                return App.API.getBitcoinRatesForOneDay();
+            case 'ONE_HOUR':
+                return App.API.getBitcoinRatesForOneHour();
+        }
+    },
+
+    getLabelFormat(period) {
+        switch(period) {
+            case 'ALL': return 'YYYY';
+            case 'ONE_YEAR': return 'MMM YYYY';
+            case 'ONE_MONTH': return 'Do MMM';
+            case 'ONE_WEEK': return 'dddd';
+            case 'ONE_DAY': return 'HH:mm';
+            case 'ONE_HOUR': return 'HH:mm';
+        }
+    },
+
+    repositories: {},
+    initRepositories() {
+        Object.keys(this.PERIODS).forEach( period =>
+            this.repositories[period] = new SuperRepo({
+                storage: 'BROWSER_STORAGE',
+                name: 'bitcoin-' + period,
+                outOfDateAfter: 15 * 60 * 1000,
+                mapData: r => App.API.mapData(r, this.getLabelFormat(period)),
+                request: () => this.getBitcoinData(period)
+            })
+        );
+    },
+
+    init() {
+        this.chart = new App.Chart(this.$chart);
+        this.initRepositories();
+
+
+        this.initEvents();
+    }
+};
+
+window.App.Bitcoin.init();
--- js/script.js
+++ js/script.js
@@ -8,69 +8,11 @@ const PERIODS = {
 };
 
 $(function(){
-    // Init Chart
-    const el = document.getElementById('chart');
-    const chart = new App.Chart(el);
-
     // Init Clock
     const clock = new App.Clock();
 
-    $dataPeriods = $('.js-period');
-
-    $dataPeriods.on('click', function() {
-        $('#periods').find('.active').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    function getBitcoinData(period) {
-        switch(period) {
-            case 'ALL':
-                return App.API.getBitcoinRatesForAll();
-            case 'ONE_YEAR':
-                return App.API.getBitcoinRatesForOneYear();
-            case 'ONE_MONTH':
-                return App.API.getBitcoinRatesForOneMonth();
-            case 'ONE_WEEK':
-                return App.API.getBitcoinRatesForOneWeek();
-            case 'ONE_DAY':
-                return App.API.getBitcoinRatesForOneDay();
-            case 'ONE_HOUR':
-                return App.API.getBitcoinRatesForOneHour();
-        }
-    }
-
-    function getLabelFormat(period) {
-        switch(period) {
-            case 'ALL': return 'YYYY';
-            case 'ONE_YEAR': return 'MMM YYYY';
-            case 'ONE_MONTH': return 'Do MMM';
-            case 'ONE_WEEK': return 'dddd';
-            case 'ONE_DAY': return 'HH:mm';
-            case 'ONE_HOUR': return 'HH:mm';
-        }
-    }
-
-    const BitcoinRepository = {};
-    Object.keys(PERIODS).forEach( period =>
-        BitcoinRepository[period] = new SuperRepo({
-            storage: 'BROWSER_STORAGE',
-            name: 'bitcoin-' + period,
-            outOfDateAfter: 15 * 60 * 1000,
-            mapData: r => App.API.mapData(r, getLabelFormat(period)),
-            request: () => getBitcoinData(period)
-        })
-    );
-
-    $dataPeriods.on('click', function(){
-        const period = $(this).data('period');
-
-        BitcoinRepository[period].getData()
-            .then(_data => chart.init(_data));
-    });
-
-    $dataPeriods.eq(1).trigger('click');
-
     // Now
+    const BitcoinRepository = {}
     BitcoinRepository['NOW'] = new SuperRepo({
         storage: 'BROWSER_STORAGE',
         name: 'bitcoin-now',
@@ -81,6 +23,8 @@ $(function(){
         mapData: data => ({ price: data[0].value }),
         request: () => App.API.getBitcoinRatesNow()
     });
+
+    const lastUpdated = document.querySelector('#last-updated');
 
     BitcoinRepository['NOW'].getData().then( _data => {
         let priceNow = _data.price;
@@ -96,8 +40,6 @@ $(function(){
             lastUpdated.textContent = moment(info.lastFetched).fromNow();
         });
     });
-
-    const lastUpdated = document.querySelector('#last-updated');
 
     BitcoinRepository['NOW'].getDataUpToDateStatus().then(info => {
         lastUpdated.textContent = moment(info.lastFetched).fromNow();
