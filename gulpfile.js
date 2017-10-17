e35600cf226ee991e283aa38824788beece8e43b
superKalo <superkalo@devlabs.bg>
2017-10-17 18:36:40 +0300

    Add: copy npm deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
+const gnf = require('gulp-npm-files');
 
-gulp.task('copy:extension', function () {
+gulp.task('copy-files:extension', function () {
     gulp.src([
         './src/index.html',
         './src/manifest.json',
@@ -8,3 +9,10 @@ gulp.task('copy:extension', function () {
         './src/css/**/*',
     ], { base: './src' }).pipe(gulp.dest('./dist/extension/'));
 });
+
+// Copy dependencies to build/node_modules/
+gulp.task('copy-npm-dependencies:extension', function() {
+    gulp.src(gnf(), {base:'./'}).pipe(gulp.dest('./dist/extension'));
+});
+
+gulp.task('build:extension', ['copy-files:extension', 'copy-npm-dependencies:extension']);
