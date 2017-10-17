4d6322a07dbba8f602fc110e76d596c70b3a44e8
superKalo <superkalo@devlabs.bg>
2017-10-17 18:59:27 +0300

    Add: env variables!

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 const gulp = require('gulp');
 const gnf = require('gulp-npm-files');
+const rename = require('gulp-rename');
 
 gulp.task('copy-files:extension', function () {
     gulp.src([
         './src/index.html',
         './src/manifest.json',
         './src/js/**/*',
-        './src/css/**/*',
+        './src/css/**/*'
     ], { base: './src' }).pipe(gulp.dest('./dist/extension/'));
 });
+
+gulp.task('set-env', function () {
+    gulp.src(`./src/env/${process.env.ENV}.env.js`)
+        .pipe(rename('env.js'))
+        .pipe(gulp.dest('./dist/extension/env/'));
+});
+
+
 
 // Copy dependencies to build/node_modules/
 gulp.task('copy-npm-dependencies:extension', function() {
     gulp.src(gnf(), {base:'./'}).pipe(gulp.dest('./dist/extension'));
 });
 
-gulp.task('build:extension', ['copy-files:extension', 'copy-npm-dependencies:extension']);
+gulp.task('build:extension', ['copy-files:extension', 'copy-npm-dependencies:extension', 'set-env']);
