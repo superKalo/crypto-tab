8f1640be2de42015a5a77973ec1220ef52327491
superKalo <superkalo@devlabs.bg>
2017-10-18 15:37:20 +0300

    Add: chrome and ff download buttons for the website only; Add: option to add platform specific DOM

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,12 @@
 window.App.Bitcoin.init();
 
-window.onload = () => window.App.Loader.displayPage();
+window.onload = () => {
+    const { platform } = App.ENV;
+
+    // Display platform specific DOM elements
+    [...document.querySelectorAll(`[data-platform="${platform}"]`)].forEach( el => {
+        el.classList.remove('hidden');
+    });
+
+    window.App.Loader.displayPage();
+};
